c9dc9be0851dcd817b10df869be1def20f7c99b0
Julien Martin <m@rtin.tech>
2024-12-21 22:58:41 +0100

    Sections are sorted by convention

--- generator/index.js
+++ generator/index.js
@@ -2,14 +2,14 @@ import path from 'node:path';
 import fs from 'node:fs/promises';
 
 import Filter from './entities/filter.js';
-import sections from './sections/index.js';
+import loadSections from './sections/index.js';
 
 import { FILTER_NAME } from './configuration.js';
 
-const filter = new Filter();
-filter.addSections(sections);
-
 (async () => {
+  const filter = new Filter();
+  filter.addSections(await loadSections());
+
   const fileName = `${FILTER_NAME}.filter`;
   await fs.writeFile(path.resolve('..', fileName), `${filter}`, 'utf-8');
 })();
--- generator/sections/index.js
+++ generator/sections/index.js
@@ -1,40 +1,25 @@
-import { DEBUG } from '../configuration.js';
-import defaultSection from './default.js';
-import gold from './gold.js';
-import sekhemasTrial from './sekhemas-trial.js';
-import expeditions from './expeditions.js';
-import uniques from './uniques.js';
-import amuletsRingsBelts from './amulets-rings-belts.js';
-import charms from './charms.js';
-import runes from './runes.js';
-import flasks from './flasks.js';
-import ultimatum from './ultimatum.js';
-import gems from './gems.js';
-import jewels from './jewels.js';
-import currencies from './currencies.js';
-import waystones from './waystones.js';
-import magicRares from './magic-rares.js';
-import salvageables from './salvageables.js';
-import useless from './useless.js';
-import debug from './debug.js';
+import fs from 'node:fs/promises';
+import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
 
-export default [
-  defaultSection,
-  gold,
-  uniques,
-  sekhemasTrial,
-  ultimatum,
-  expeditions,
-  amuletsRingsBelts,
-  charms,
-  runes,
-  flasks,
-  gems,
-  jewels,
-  currencies,
-  waystones,
-  magicRares,
-  salvageables,
-  useless,
-  DEBUG && debug,
-].filter(Boolean);
+import { DEBUG } from '../configuration.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = join(__filename, '../');
+
+const loadSections = async () => {
+  const sectionFiles = (await fs.readdir(__dirname))
+    .filter((file) => file.endsWith('.js') && file !== 'index.js')
+    .sort();
+
+  const sections = await Promise.all(
+    sectionFiles.map(async (file) => {
+      const module = await import(join(__dirname, file));
+      return module.default;
+    }),
+  );
+
+  return DEBUG ? sections : sections.filter((section) => section.name !== 'debug');
+};
+
+export default loadSections;
