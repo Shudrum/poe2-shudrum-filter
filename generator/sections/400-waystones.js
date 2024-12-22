048074bf30f7e7f694ea480cb209867defc7909b
Julien Martin <Shudrum@users.noreply.github.com>
2024-12-22 18:40:09 +0100

    Fixed Tablet filtering
    Issue with the section setCommon. This feature should be removed.

--- generator/sections/400-waystones.js
+++ generator/sections/400-waystones.js
@@ -9,11 +9,12 @@ import { MINIMUM_AREA_LEVEL, COLORS, VARIABLES, THEMES } from '../configuration.
 export default (mode) => {
   const section = new Section('Waystones');
 
-  section.setCommon({
+  const common = {
     class: 'Waystone',
-  });
+  };
 
   section.addBlock(new Block({
+    ...common,
     comment: 'Common',
     card: new Card(THEMES.WAYSTONES, Card.SIZES.MEDIUM, Card.TYPES.OUTLINE),
     sound: new Sound(Sound.TYPES.WAYSTONE),
@@ -23,6 +24,7 @@ export default (mode) => {
 
   for (let waystoneTier = 1; waystoneTier <= 20; waystoneTier++) {
     section.addBlock(new Block({
+      ...common,
       areaLevel: `== ${MINIMUM_AREA_LEVEL + (waystoneTier - 1)}`,
       waystoneTier: `>= ${waystoneTier}`,
       card: new Card(THEMES.WAYSTONES, Card.SIZES.BIG, Card.TYPES.IMPORTANT),
@@ -35,6 +37,7 @@ export default (mode) => {
 
     if (waystoneTier >= 2) {
       section.addBlock(new Block({
+        ...common,
         areaLevel: `== ${MINIMUM_AREA_LEVEL + (waystoneTier - 1)}`,
         waystoneTier: `< ${waystoneTier}`,
         card: new Card(THEMES.WAYSTONES, Card.SIZES.MEDIUM, Card.TYPES.OUTLINE),
@@ -50,6 +53,7 @@ export default (mode) => {
 
     if (waystoneTier >= VARIABLES.WAYSTONES.HIDE_GAP[mode] + 1) {
       section.addBlock(new Block({
+        ...common,
         visible: false,
         areaLevel: `== ${MINIMUM_AREA_LEVEL + (waystoneTier - 1)}`,
         waystoneTier: `<= ${waystoneTier - VARIABLES.WAYSTONES.HIDE_GAP[mode]}`,
