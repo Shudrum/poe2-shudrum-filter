65a684248020e2ab15a5972e6d6e4ca71c735e2a
Julien Martin <Shudrum@users.noreply.github.com>
2024-12-22 15:45:44 +0100

    Useless section fixed

--- generator/sections/902-useless.js
+++ generator/sections/902-useless.js
@@ -5,28 +5,37 @@ import { MINIMUM_AREA_LEVEL, VARIABLES } from '../configuration.js';
 export default (mode) => {
   const section = new Section('Useless items');
 
+  const classes = [
+    'Boots',
+    'QuarterStaff',
+    'Crossbow',
+    'Body Armour',
+    'Two Hand Mace',
+    'Gloves',
+    'Focus',
+    'Wand',
+    'One Hand Mace',
+    'Helmet',
+    'Shield',
+    'Quiver',
+    'Sceptre',
+    'Bow',
+    'Staff',
+  ];
+
   section.addBlock(new Block({
     visible: false,
-    class: [
-      'Boots',
-      'QuarterStaff',
-      'Crossbow',
-      'Body Armour',
-      'Two Hand Mace',
-      'Gloves',
-      'Focus',
-      'Wand',
-      'One Hand Mace',
-      'Helmet',
-      'Shield',
-      'Quiver',
-      'Sceptre',
-      'Bow',
-      'Staff',
-    ],
-    rarity: '<= Magic',
+    class: classes,
+    rarity: 'Magic',
     areaLevel: `>= ${MINIMUM_AREA_LEVEL}`,
     itemLevel: `< ${VARIABLES.MIN_ITEM_LEVEL_NORMAL_MAGIC_RARE[mode]}`,
+  }));
+
+  section.addBlock(new Block({
+    visible: false,
+    class: classes,
+    rarity: 'Normal',
+    areaLevel: `>= ${MINIMUM_AREA_LEVEL}`,
   }));
 
   return section;
