10e4b4f7bce7b64926070132f6d492ab539bad07
Julien Martin <Shudrum@users.noreply.github.com>
2024-12-22 12:44:51 +0100

    Filters are deployed to PoE 2 folder when dev started

--- generator/index.js
+++ generator/index.js
@@ -5,12 +5,27 @@ import { pascalCase } from 'change-case';
 
 import Filter from './entities/filter.js';
 import loadSections from './sections/index.js';
-
 import { MODES, FILTER_NAME } from './configuration.js';
 
 (async () => {
   const sections = await loadSections();
-  const header = await fs.readFile('header.txt', 'utf-8');
+  const header = await fs.readFile(path.resolve('./header.txt'), 'utf-8');
+  const gameDirectory = path.join(
+    process.env.USERPROFILE,
+    'Documents',
+    'My Games',
+    'Path of Exile 2',
+  );
+
+  let deployFilters = false;
+  if (process.env.NODE_ENV === 'development') {
+    if (!(await fs.stat(gameDirectory)).isDirectory()) {
+      console.log('Path of Exile 2 seems to not be installed. Skipping the filters deployment');
+    } else {
+      console.log('Path of Exile 2 in installed. Filters are goind to be deployed');
+      deployFilters = true;
+    }
+  }
 
   await Promise.all(Object.values(MODES).map(async (mode) => {
     const filter = new Filter(mode, header);
@@ -18,5 +33,8 @@ import { MODES, FILTER_NAME } from './configuration.js';
 
     const fileName = `${FILTER_NAME}${pascalCase(mode)}.filter`;
     await fs.writeFile(path.resolve('..', fileName), `${filter}`, 'utf-8');
+    if (deployFilters) {
+      await fs.writeFile(path.resolve(gameDirectory, fileName), `${filter}`, 'utf-8');
+    }
   }));
 })();
