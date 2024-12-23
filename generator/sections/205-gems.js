ae056bbf73ed43d365c7f27b79ab83a5a3ab1208
Shudrum <m@rtin.tech>
2024-12-23 18:04:06 +0100

    Support gems display tweaked

--- generator/sections/205-gems.js
+++ generator/sections/205-gems.js
@@ -1,12 +1,32 @@
 import { Section } from '../entities/filter/index.js';
 import { Card, Effect, MapIcon, Sound } from '../entities/generators/index.js';
 
+// TODO: Spirit, Uncut and Skill gems should have a different display.
+// TODO: A proper loop should be defined to remove progressively the gems.
+
+// Area 77 / T12 / 17
+
 export default () => {
   const section = Section('Gems');
 
-  // TODO: Spirit, Uncut and Skill gems should have a different display.
   section.addBlock({
-    type: ['Uncut Spirit Gem', 'Uncut Support Gem', 'Uncut Skill Gem'],
+    type: ['Uncut Support Gem'],
+    card: Card(Card.THEMES.GEMS, Card.SIZES.MEDIUM, Card.TYPES.OUTLINE),
+    icon: MapIcon(MapIcon.SIZES.SMALL, MapIcon.COLORS.CYAN, MapIcon.SHAPES.CIRCLE),
+    effect: Effect(Effect.COLORS.CYAN, Effect.TEMPORARY),
+    sound: Sound(Sound.TYPES.GEMS),
+  });
+
+  section.addBlock({
+    type: ['Uncut Spirit Gem'],
+    card: Card(Card.THEMES.GEMS, Card.SIZES.BIG, Card.TYPES.IMPORTANT),
+    icon: MapIcon(MapIcon.SIZES.BIG, MapIcon.COLORS.CYAN, MapIcon.SHAPES.CIRCLE),
+    effect: Effect(Effect.COLORS.CYAN),
+    sound: Sound(Sound.TYPES.GEMS),
+  });
+
+  section.addBlock({
+    type: ['Uncut Skill Gem'],
     card: Card(Card.THEMES.GEMS, Card.SIZES.MEDIUM, Card.TYPES.IMPORTANT),
     icon: MapIcon(
       MapIcon.SIZES.BIG,
@@ -17,7 +37,6 @@ export default () => {
     sound: Sound(Sound.TYPES.GEMS),
   });
 
-  // TODO: A proper loop should be defined to remove progressively the gems.
   section.addBlock({
     type: 'Uncut Skill Gem',
     visible: false,
