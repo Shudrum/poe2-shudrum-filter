ef7aa42644bcee5a0bb0a42025913aea45c69c1b
Julien Martin <m@rtin.tech>
2024-12-30 10:38:56 +0100

    Important items added

--- generator/configuration/important-items.js
+++ generator/configuration/important-items.js
@@ -1,1 +1,15 @@
-export default [];
+export default [{
+  class: ['Currency'],
+  type: [
+    'Orb of Chance',
+    'Divine Orb',
+    'Mirror',
+    'Greater Jeweller\'s Orb',
+    'Perfect Jeweller\'s Orb',
+    'Orb of Annulment',
+  ],
+}, {
+  type: ['Distilled Isolation', 'Distilled Suffering'],
+}, {
+  class: ['Pinnacle Keys'],
+}];
